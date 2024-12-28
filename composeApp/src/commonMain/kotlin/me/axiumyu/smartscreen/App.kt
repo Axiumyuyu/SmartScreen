d0b4efe3ee891ef47a566a9e55685b9588bb4133
Axium_yu <1738884091@qq.com>
2024-12-28 08:25:05 +0800

    fix code

--- composeApp/src/commonMain/kotlin/me/axiumyu/smartscreen/App.kt
+++ composeApp/src/commonMain/kotlin/me/axiumyu/smartscreen/App.kt
@@ -2,18 +2,18 @@ package me.axiumyu.smartscreen
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
 import smartscreen.composeapp.generated.resources.Res
 import smartscreen.composeapp.generated.resources.compose_multiplatform
 
